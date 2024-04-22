afff97960f85116f9786b3e047cd4ef2d6ce1a04
drianodev <adrianomendes661@gmail.com>
2024-04-22 14:43:10 -0300

    Implementação da camada de dominio

---
+++ src/main/java/br/com/drianodev/santanderweek/domain/model/Feature.java
@@ -0,0 +1,7 @@
+package br.com.drianodev.santanderweek.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem {
+}
---
+++ src/main/java/br/com/drianodev/santanderweek/domain/model/News.java
@@ -0,0 +1,7 @@
+package br.com.drianodev.santanderweek.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem {
+}
